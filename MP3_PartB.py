753883682610ba188a98d98a89dbc7d614379cb3
Samuel Valenzuela <Sam@Samuels-MacBook-Pro.local>
2020-05-01 20:25:09 +0200

    8

--- MP3_PartB.py
+++ MP3_PartB.py
@@ -22,7 +22,7 @@ schema = StructType([StructField("word", StringType(), True),
 
 df = sqlContext.read.csv('gbooks', schema=schema, sep='\t')
 
-df.show(10)
+df.count()
 
 # Spark SQL - DataFrame API
 
