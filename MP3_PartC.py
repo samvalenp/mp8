1bc38dad4928f719503838f3417df740cabca285
Samuel Valenzuela <Sam@Samuels-MacBook-Pro.local>
2020-05-01 20:54:18 +0200

    11

--- MP3_PartC.py
+++ MP3_PartC.py
@@ -33,7 +33,7 @@ df = sqlContext.read.csv('gbooks', schema=schema, sep='\t')
 #df.word.like("ATTRIBUTE").show(10)
 
 df.createOrReplaceTempView("tableTemp")
-sqlContext.sql("SELECT * from tableTemp where count1 > 1900 ").show(10)
+sqlContext.sql("SELECT * from tableTemp where word ='ATTRIBUTE'").show(10)
 # Spark SQL
 
 # +--------+                                                                      
